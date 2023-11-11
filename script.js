60c8b8fc650a52f8a1417248c9b8c3d490f2276f
erlams <elmaddin@gmail.com>
2023-11-11 15:00:37 +0100

    s

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ let netica;
 
 
 
-let city1 = prompt(' Hardan gedirsiz? ')
+
+
+if (icaze == true) { 
+    netica = 'Siz masini sure bilersiz!'
+    let city1 = prompt(' Hardan gedirsiz? ')
 let city2 = prompt(' Hara gedirsiz? ')
 
 let mesafe = prompt( 'Mesafe nedi');
 let suret = prompt( 'Suret nedi');
 let vaxt = mesafe / suret ;
 let netice = city1 + " şəhərindən " + city2 + " şəhərinə məsafə " +  mesafe + " km, suret " + suret + " km/saat, vaxt " + vaxt + "  saat olacaq";
-
-if (icaze == true) { 
-    netica = 'Siz masini sure bilersiz!'
 }
 else {
     netica = ' Sizin yasiniz catmir!'
