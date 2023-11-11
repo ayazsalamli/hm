737fbfd114ced3bcba11aba815f72ed442a89cf7
erlams <elmaddin@gmail.com>
2023-11-11 14:54:45 +0100

    m

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-alert("Hello");
+alert("Xoş gəlmisiniz!");
 
-let icaze = confirm('Yasiniz 18-den yuxaridir mi?');
+let icaze = confirm('Yaşınız 18-den yuxarıdırmı?');
 let netica;
 
 
 
 
 
-let city1 = prompt(' haradan gedirsiz ')
-let city2 = prompt(' hara gedirsiz ')
+let city1 = prompt(' Hardan gedirsiz? ')
+let city2 = prompt(' Hara gedirsiz? ')
 
 let mesafe = prompt( 'Mesafe nedi');
 let suret = prompt( 'Suret nedi');
 let vaxt = mesafe / suret ;
-let netice = city1 + " şəhərindən " + city2 + " şəhərinə məsafə " +  mesafe + " km " + suret + " km/saat " + vaxt + "  saat olacaq";
+let netice = city1 + " şəhərindən " + city2 + " şəhərinə məsafə " +  mesafe + " km, suret " + suret + " km/saat, vaxt " + vaxt + "  saat olacaq";
 
 if (icaze == true) { 
-    netica = 'Siz masini sure bilersiz'
+    netica = 'Siz masini sure bilersiz!'
 }
 else {
-    netica = ' Sizin yasiniz catmir'
+    netica = ' Sizin yasiniz catmir!'
 }
 
-document.getElementById('murad').innerHTML = netice;
+document.getElementById('ayaz').innerHTML = netice;
 
-document.getElementById('ayaz').innerHTML = netice
